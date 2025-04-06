4f914f7d6295c562e2e55bdd3258c1b345ecf4d7
kulikp1 <pashakylik2@gmail.com>
2025-04-06 23:11:09 +0300

    add route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Calendar from "../Calendar/Calendar";
 import styles from "./App.module.css";
 
 const App = () => {
   return (
-    <div className={styles.container}>
-      <Calendar />
-    </div>
+    <Router>
+      <div className={styles.container}>
+        <Routes>
+          <Route path="/" element={<Calendar />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
