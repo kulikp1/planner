3932169286007a28aee8ac7fbbeef3436bd9ed94
kulikp1 <pashakylik2@gmail.com>
2025-04-07 22:30:37 +0300

    update default styles and add content to events.jsx

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,5 +1,22 @@
-const Events = () => {
-  return <h1>Events</h1>;
+import { events } from "../../utils/events";
+import styles from "./Events.module.css";
+
+const UpcomingEvents = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Події у Чернігові</h1>
+
+      <div className={styles.eventsList}>
+        {events.map((event) => (
+          <div key={event.id} className={styles.eventCard}>
+            <h2 className={styles.eventTitle}>{event.title}</h2>
+            <p className={styles.eventDate}>{event.date}</p>
+            <p className={styles.eventLocation}>{event.location}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Events;
+export default UpcomingEvents;
