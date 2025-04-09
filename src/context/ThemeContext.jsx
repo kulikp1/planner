69e947ff11a3f459d1de31966b5ca9dab3fd0aa6
kulikp1 <pashakylik2@gmail.com>
2025-04-09 22:52:35 +0300

    Update ThemeContext.jsx

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -23,4 +23,5 @@ export const ThemeProvider = ({ children }) => {
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useTheme = () => useContext(ThemeContext);
