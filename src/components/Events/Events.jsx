5098549771ab8ccd26446c26d4abce768951e24f
kulikp1 <pashakylik2@gmail.com>
2025-04-10 15:43:31 +0300

    update event title for light theme

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -88,7 +88,13 @@ const UpcomingEvents = () => {
                 isDarkMode ? styles.eventCard : styles.eventCardLight
               }`}
             >
-              <h2 className={styles.eventTitle}>{event.title}</h2>
+              <h2
+                className={` ${
+                  isDarkMode ? styles.title : styles.eventLightTitle
+                }`}
+              >
+                {event.title}
+              </h2>
               <p className={styles.eventDate}>{event.date}</p>
               <p className={styles.eventLocation}>{event.location}</p>
               <button
