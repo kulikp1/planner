dfa5d401328e6428620421f2160a744380d728bf
kulikp1 <pashakylik2@gmail.com>
2025-04-10 16:19:44 +0300

    add light theme

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,9 +2,11 @@ import { useNavigate } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import styles from "./Home.module.css";
 import logo from "../../assets/Logo.png";
+import { useTheme } from "../../context/ThemeContext"; // імпорт контексту теми
 
 export default function Home() {
   const navigate = useNavigate();
+  const { isDarkMode } = useTheme(); // використовуємо тему
 
   return (
     <div className={styles.container}>
@@ -13,11 +15,16 @@ export default function Home() {
         <div className={styles.left}>
           <img src={logo} alt="Logo" className={styles.logo} />
         </div>
-        <div className={styles.right}>
+        <div className={`${styles.right} ${!isDarkMode ? styles.light : {}}`}>
           <h1>
             Онлайн планер <br /> Твій зручний тайм-менеджмент помічник
           </h1>
-          <button onClick={() => navigate("/calendar")}>
+          <button
+            className={`${styles.rightButton} ${
+              !isDarkMode ? styles.light : {}
+            }`}
+            onClick={() => navigate("/calendar")}
+          >
             Перейти до календаря
           </button>
         </div>
